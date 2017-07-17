3c720ee2aa8d6745cc88fdcaae1edb37cc066b0e
Roshan Ravi <roshanravi10@gmail.com>
2017-07-17 17:40:46 -0400

    Edits

---
+++ AnyPrint/AnyPrint/CameraStreamView.swift
@@ -0,0 +1,24 @@
+//
+//  CameraStreamView.swift
+//  AnyPrint
+//
+//  Created by Roshan Ravi on 7/17/17.
+//  Copyright © 2017 Roshan Ravi. All rights reserved.
+//
+
+import UIKit
+
+class CameraStreamView: UIView {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // Ref: https://stackoverflow.com/questions/9282365/load-view-from-an-external-xib-file-in-storyboard
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let view = Bundle.main.loadNibNamed("CameraStreamView", owner: self, options: nil)!.first as! UIView
+        
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+
+}
