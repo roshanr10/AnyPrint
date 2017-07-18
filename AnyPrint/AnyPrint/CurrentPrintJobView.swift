b46ba042b64adf9cfafa729ed29638010b8babeb
Roshan Ravi <roshanravi10@gmail.com>
2017-07-17 23:29:55 -0400

    Current Print Job View

---
+++ AnyPrint/AnyPrint/CurrentPrintJobView.swift
@@ -0,0 +1,11 @@
+//
+//  CurrentPrintJobView.swift
+//  AnyPrint
+//
+//  Created by Roshan Ravi on 7/17/17.
+//  Copyright © 2017 Roshan Ravi. All rights reserved.
+//
+
+import UIKit
+
+class CurrentPrintJobView: CustomUIView {}
