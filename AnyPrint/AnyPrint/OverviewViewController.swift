b0b114e89ef2fcc2ebc215fa78224744b4dcfab5
Roshan Ravi <roshanravi10@gmail.com>
2017-08-01 18:08:27 -0400

    Bootstrap for State View
    Only requires Dict->Class conversion for state to work

--- AnyPrint/AnyPrint/OverviewViewController.swift
+++ AnyPrint/AnyPrint/OverviewViewController.swift
@@ -11,9 +11,27 @@ import UIKit
 class OverviewViewController: UIViewController {
     @IBOutlet weak var cameraStream: MJPGCameraStreamView!
     
+    @IBOutlet weak var printerName: UILabel!
+    @IBOutlet weak var printerStatus: UILabel!
+    @IBOutlet weak var hotend: ToolTemperatureView!
+    @IBOutlet weak var bed: ToolTemperatureView!
+    @IBOutlet weak var currentJob: CurrentPrintJobView!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         cameraStream.cameraConfig = Printers.sharedInstance.selected?.config.camera
+        
+        printerName.text = Printers.sharedInstance.selected?.config.name
+        printerStatus.text = Printers.sharedInstance.selected?.state?.state
+        
+        if let tools = Printers.sharedInstance.selected?.state?.tools,
+            tools.count > 0 {
+            hotend.state = tools[0]
+        }
+        
+        bed.state = Printers.sharedInstance.selected?.state?.bed
+        
+        currentJob.currentPrintJob = Printers.sharedInstance.selected?.state?.job
     }
 }
